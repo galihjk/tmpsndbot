d543256d6096bee83fa22b77d8fd6e391cac7f14
Galih JK <galih.jk@gmail.com>
2024-02-07 23:51:18 +0700

    exception

--- webhook_a3c59a7ed8d365cf267ef5cc4aca0c17.php
+++ webhook_a3c59a7ed8d365cf267ef5cc4aca0c17.php
@@ -1,12 +1,22 @@
 <?php
 include("init.php");
+try {
+    $jenis_update = [
+        "message",
+        "callback_query",
+        "my_chat_member",
+    ];
 
-$jenis_update = [
-    "message",
-    "callback_query",
-    "my_chat_member",
-];
+    f("handle_update_sesuai_jenis")($jenis_update);
 
-f("handle_update_sesuai_jenis")($jenis_update);
-
-f("db_disconnet")();
+    f("db_disconnet")();
+}
+catch (Exception $e) {
+    foreach(f("get_config")("bot_admins",[]) as $chatidadmin){
+        f("bot_kirim_perintah")("sendMessage",[
+            'chat_id'=>$chatidadmin,
+            'text'=>$e->getMessage(),
+        ]);
+    };
+    file_put_contents("log/Last Error PHP.txt",print_r($e,true));
+}
