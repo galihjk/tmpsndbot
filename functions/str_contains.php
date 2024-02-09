f3abcb1574da4b064d1e53b12445e00cd89e5b48
Galih JK <galih.jk@gmail.com>
2024-02-09 12:48:41 +0700

    str contains

--- functions/str_contains.php
+++ functions/str_contains.php
@@ -1,7 +1,9 @@
 <?php
-// function str_contains($haystack, $needle){
-//     if((string)$needle === ""){
-//         return true;
-//     }
-//     return (strpos($haystack, $needle) !== false);
-// }
+if(!function_exists("str_contains")){
+    function str_contains($haystack, $needle){
+        if((string)$needle === ""){
+            return true;
+        }
+        return (strpos($haystack, $needle) !== false);
+    }
+}
