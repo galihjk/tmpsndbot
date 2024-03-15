51b40856ed6b99564bfd48b3965b98b0ee30d437
Galih JK <galih.jk@gmail.com>
2024-03-16 04:37:42 +0700

    webhookOK

--- functions/webhook_ok.php
+++ functions/webhook_ok.php
@@ -1,8 +1,5 @@
 <?php
 function webhook_ok(){
-    f("bot_kirim_perintah")("sendMessage",[
-        'chat_id'=>'227024160',
-        'text'=>f("get_config")("channel"),
-    ]);
+    echo "OK";
 }
     
